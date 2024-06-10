9a89d0e9c7a178cab3626298662971c87e4dfc53
JulienThevenoz <julien@thevenoz.ch>
2024-06-10 14:45:32 +0200

    revert hello world change

--- crazyflie_examples/crazyflie_examples/hello_world.py
+++ crazyflie_examples/crazyflie_examples/hello_world.py
@@ -12,6 +12,7 @@ def main():
     timeHelper = swarm.timeHelper
     cf = swarm.allcfs.crazyflies[0]
     cf.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
+    
     timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
     cf.land(targetHeight=0.04, duration=2.5)
     timeHelper.sleep(TAKEOFF_DURATION)
